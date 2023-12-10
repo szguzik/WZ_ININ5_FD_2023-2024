f92cbb671c6d8258eb1431f26904e532f424e6b3
Szymon Guzik <szymon.guzik@cyfrowe.pl>
2023-12-10 13:43:29 +0100

    otwieranie i zamykanie czatu

--- app.vue
+++ app.vue
@@ -1,3 +1,9 @@
+<script setup>
+const openChat = ref(false)
+const toggle = () => {
+    openChat.value = !openChat.value
+}
+</script>
 <template>
   <div>
   <NuxtLayout>
@@ -5,15 +11,26 @@
       <NuxtPage/>
     </div>
   </NuxtLayout>
-  <div class="circle"></div>
+  <div class="chatOpen" @click="toggle" v-if="openChat"></div>
+  <div class="chatClose" @click="toggle" v-else></div>
+  
   </div>
 </template>
 <style scoped>
-  .circle {
+  .chatClose {
     width: 50px;
     height: 50px;
     background-color: #3498db; /* Kolor tła */
     border-radius: 50%; /* Ustala kształt koła */
+    position: absolute; /* Pozycja absolutna */
+    bottom: 0; /* Odległość od dołu */
+    right: 0; /* Odległość od prawej */
+  }
+  
+  .chatOpen {
+    width: 50px;
+    height: 150px;
+    background-color: #3498db; /* Kolor tła */
     position: absolute; /* Pozycja absolutna */
     bottom: 0; /* Odległość od dołu */
     right: 0; /* Odległość od prawej */
