2dc777985ec8b8bb921b26d87ccd0d3d775b6002
Szymon Guzik <szymon.guzik@cyfrowe.pl>
2023-12-10 13:30:57 +0100

    dodanie  "czatu" bez wzgledu na layout i stronę

--- app.vue
+++ app.vue
@@ -5,5 +5,17 @@
       <NuxtPage/>
     </div>
   </NuxtLayout>
+  <div class="circle"></div>
   </div>
 </template>
+<style scoped>
+  .circle {
+    width: 50px;
+    height: 50px;
+    background-color: #3498db; /* Kolor tła */
+    border-radius: 50%; /* Ustala kształt koła */
+    position: absolute; /* Pozycja absolutna */
+    bottom: 0; /* Odległość od dołu */
+    right: 0; /* Odległość od prawej */
+  }
+</style>
